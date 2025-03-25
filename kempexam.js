29abd1003c68725ddece249ecd2857f9f41a0b1e
EngineerKemp <kempsimeonramores@gmail.com>
2025-03-25 16:39:08 +0800

    sanagumanaulit

---
+++ kempexam.js
@@ -0,0 +1,5 @@
+document.getElementById('changeStyleBtn').addEventListener('click', function() 
+{
+    const table = document.getElementById('studentTable');
+    table.classList.toggle('newStyle');
+});
